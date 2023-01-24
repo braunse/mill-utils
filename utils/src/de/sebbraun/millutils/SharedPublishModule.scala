1a70320365cfc5edd7f136478bbe1da12be7e34e
Sebastien Braun <sebastien@sebbraun.de>
2023-01-24 09:51:49 +0100

    Fix moduleDep typings

--- utils/src/de/sebbraun/millutils/SharedPublishModule.scala
+++ utils/src/de/sebbraun/millutils/SharedPublishModule.scala
@@ -3,23 +3,28 @@ package de.sebbraun.millutils
 import mill._
 import mill.scalalib._
 import mill.scalalib.publish._
+import mill.scalajslib._
 
-trait SharedPublishModule extends Module {
-  outer: SharedModule =>
-
+trait SharedPublishModule extends SharedModule { outer =>
   def artifactName: T[String]
   def pomSettings: T[PomSettings]
   def publishVersion: T[String]
 
-  trait FrontendPublishModule extends PublishModule {
+  override def moduleDeps: Seq[SharedPublishModule]       = Seq()
+  override def backendModuleDeps: Seq[PublishModule]      = Seq()
+  override def frontendModuleDeps: Seq[PublishModule]     = Seq()
+
+  trait FrontendPublishModule extends FrontendModule with PublishModule {
     override def artifactName = T{ outer.artifactName() }
     def publishVersion = T{ outer.publishVersion() }
     def pomSettings = T{ outer.pomSettings() }
+    override def moduleDeps: Seq[ScalaJSModule with PublishModule] = super.moduleDeps.map(_.asInstanceOf[ScalaJSModule with PublishModule])
   }
 
   trait BackendPublishModule extends PublishModule {
     override def artifactName = T{ outer.artifactName() }
     def publishVersion = T{ outer.publishVersion() }
     def pomSettings = T{ outer.pomSettings() }
+    override def moduleDeps: Seq[ScalaModule with PublishModule] = super.moduleDeps.map(_.asInstanceOf[ScalaModule with PublishModule])
   }
 }
